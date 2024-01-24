cbc9d10150953a2ed4a82d681451eb1ba55f7b91
Victoria Smaranda <victoria.elena01@yahoo.com>
2024-01-24 15:59:06 +0200

    feat: Created entity and controller for Microposts

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2024/06/12'],
+        ['message' => 'Hi', 'created' => '2024/07/30'],
+        ['message' => 'Bye', 'created' => '2022/09/15'],
+    ];
+
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return $this->render('hello/index.html.twig',
+        ['messages' =>
+            $this->messages,
+            'limit' => $limit
+        ]
+        );
+
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+          'hello/show_one.html.twig',
+          [
+              'message' => $this->messages[$id]
+          ]
+        );
+    }
+}
